a791f097f8761a1e23835ce4854de6c02bf9bafb
zedaeen <zentzaen@gmail.com>
2020-04-25 14:00:59 +0700

    add cors middleware

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const router = require("./router");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
